dc5675ef87e1181a31688858e7b9991a9f89b51f
WantechFan <alifiandratsaqifp@gmail.com>
2026-05-26 22:49:16 +0700

    Let's see

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
-const music = Vars.tree.loadMusic("aufdieheide"); 
+const musik = Vars.tree.loadMusic("aufdieheide"); 
 
 Events.on(ClientLoadEvent, e => { 
-    Vars.control.sound.ambientMusic.add(music); 
+    Vars.control.sound.ambientMusic.add(musik); 
+    UnitTypes.omura.weapons.get(0).shootSound = Sounds.wind3;
 });
