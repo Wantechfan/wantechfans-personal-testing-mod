95cd361a2de21cc95b17546b710e3fd9caf734f8
WantechFan <alifiandratsaqifp@gmail.com>
2026-06-19 23:52:42 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -20,15 +20,15 @@ Events.on(ClientLoadEvent, e => {
 
     // Settings Configuration
     Vars.ui.settings.addCategory("@category.mod.name", Icon.settings, table => {
-        table.checkPref("Epic Musics", Core.settings.getBool("epicMusics", false), cons(value => {
+        table.Button("Epic Musics", Core.settings.getBool("epicMusics", false), cons(value => {
             Core.settings.put("epicMusics", value);
         })).row();
         
-        table.checkPref("Scathe Cheat", Core.settings.getBool("scatheCheat", false), cons(value => {
+        table.Button("Scathe Cheat", Core.settings.getBool("scatheCheat", false), cons(value => {
             Core.settings.put("scatheCheat", value);
         })).row();
         
-        table.checkPref("Asthosus Stuff", Core.settings.getBool("asthosusStuff", false), cons(value => {
+        table.Button("Asthosus Stuff", Core.settings.getBool("asthosusStuff", false), cons(value => {
             Core.settings.put("asthosusStuff", value);
         })).row();
     });
